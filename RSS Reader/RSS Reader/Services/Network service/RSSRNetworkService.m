7f673a1e9ff01c72d896ac37932ae811df97aadb
Lina Loyko <linaloyko@icloud.com>
2020-12-07 13:34:45 +0300

    refactor: add asynchronous loading

--- RSS Reader/RSS Reader/Services/Network service/RSSRNetworkService.m
+++ RSS Reader/RSS Reader/Services/Network service/RSSRNetworkService.m
@@ -7,10 +7,10 @@
 
 #import "RSSRNetworkService.h"
 
-
 @interface RSSRNetworkService ()
 
 @property (nonatomic, retain) NSURLSession *session;
+@property (atomic, copy) void (^completion)(NSData *data, NSError *error);
 
 @end
 
@@ -25,20 +25,29 @@
 }
 
 - (void)loadDataFromURL:(NSURL *)url
-               completion:(void (^)(NSData *data, NSError *error))completion {
-    NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url
-                                                     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        if (error) {
-            completion(nil, error);
-        } else {
-            completion(data, nil);
-        }
-    }];
-    [dataTask resume];
+             completion:(void (^)(NSData *data, NSError *error))completion {
+    self.completion = completion;
+    [NSThread detachNewThreadSelector:@selector(loadDataFromURL:) toTarget:self withObject:url];
+}
+
+- (void)loadDataFromURL:(NSURL *)url {
+    @autoreleasepool {
+        __block typeof(self) weakSelf = self;
+        NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url
+                                                         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error) {
+                weakSelf.completion(nil, error);
+            } else {
+                weakSelf.completion(data, nil);
+            }
+        }];
+        [dataTask resume];
+    }
 }
 
 - (void)dealloc {
     [_session release];
+    [_completion release];
     [super dealloc];
 }
 
