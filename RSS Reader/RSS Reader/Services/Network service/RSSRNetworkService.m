cee4504164f570d4e6ecc6c40837972adc4014bb
Lina Loyko <linaloyko@icloud.com>
2021-01-24 23:05:25 +0300

    refactor: check completion block against nil prior calling it

--- RSS Reader/RSS Reader/Services/Network service/RSSRNetworkService.m
+++ RSS Reader/RSS Reader/Services/Network service/RSSRNetworkService.m
@@ -7,12 +7,16 @@
 
 #import "RSSRNetworkService.h"
 
+static NSString * const kNilCompletionMessage = @"Completion must not be nil!";
+
 @implementation RSSRNetworkService
 
 - (void)loadDataFromURL:(NSURL *)url
              completion:(void (^)(NSData *data, NSError *error))completion {
     [NSThread detachNewThreadWithBlock:^{
         @autoreleasepool {
+            NSAssert(completion, kNilCompletionMessage);
+            
             NSError *error;
             NSData *data = [NSData dataWithContentsOfURL:url options:NSDataReadingUncached error:&error];
             completion(data, error);
