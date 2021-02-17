869f37c1f8f47b7d80abbd59cf87d8ebfd256c3e
Lina Loyko <linaloyko@icloud.com>
2021-02-17 14:40:29 +0300

    fix: displaying a date on a real device

--- RSS Reader/RSS Reader/Categories/NSDate/NSDate+StringConversion.m
+++ RSS Reader/RSS Reader/Categories/NSDate/NSDate+StringConversion.m
@@ -7,16 +7,20 @@
 
 #import "NSDate+StringConversion.h"
 
+static NSString * const kLocaleIdentifier = @"en_US_POSIX";
+
 @implementation NSDate (StringConversion)
 
 - (NSString *)stringWithFormat:(NSString *)format {
     NSDateFormatter *formatter = [[NSDateFormatter new] autorelease];
+    [formatter setLocale:[NSLocale localeWithLocaleIdentifier:kLocaleIdentifier]];
     [formatter setDateFormat:format];
     return [formatter stringFromDate:self];
 }
 
 + (NSDate *)dateFromString:(NSString *)dateString withFormat:(NSString *)format {
     NSDateFormatter *formatter = [[NSDateFormatter new] autorelease];
+    [formatter setLocale:[NSLocale localeWithLocaleIdentifier:kLocaleIdentifier]];
     [formatter setDateFormat:format];
     return [formatter dateFromString:dateString];
 }
